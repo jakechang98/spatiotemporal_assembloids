92940faea1234c0c8c8b7f0b6a00c9722671d0f8
Jacob Chang <jacobchang@DN0a22f5e3.SUNet>
2025-02-03 10:43:52 -0800

    Restructure script to process CELESTA outputs for better operability with spomic.

--- analysis/process_input_data.R
+++ analysis/process_input_data.R
@@ -1,75 +1,44 @@
 devtools::load_all(path = "/Users/jacobchang/Lab/metadisco")
-source("utils/plotting_utils.R")
-library(ggplot2)
-library(ggpubr)
-library(tidyr)
 library(dplyr)
 
-base_path <- "data/assembloids/assembloids_02"
-time_points <- c("0h", "4h", "12h", "24h", "72h", "4dw", "7dw")
-time_points_numeric <- c(0, 4, 12, 24, 72, 96, 168)
-
+# Restructure data frames for use with Spomic
 process_sample <- function(fp){
-  sample = tools::file_path_sans_ext(basename(fp))
+  sample <- tools::file_path_sans_ext(basename(fp))
   df <- readxl::read_xlsx(path = fp) |> 
     dplyr::mutate(sample = sample) |> 
-    dplyr::select(sample, X, Y, "Final cell type") |> 
-    dplyr::rename(x = X, y = Y, cell_type = "Final cell type")
+    dplyr::select(sample, X, Y, "Final cell type", "Cell type number") |> 
+    dplyr::rename(x = X, y = Y, cell_type = "Final cell type", cell_type_cluster_id = "Cell type number")
   
   return(df)
 }
 
-clq_list <- list()
-for(i in seq_along(time_points)){
-  df <- process_sample(fp = paste0(base_path, "/Assembloid2_osi_", time_points[i], ".xlsx"))
-  spomic <- createSpomic(df)
-  spomic <- setSpomicHypers(
-    spomic_obj = spomic, 
-    tile_size = 250, 
-    window_size = 10, 
-    k_neigh = 15, 
-    bandwidth = 100, 
-    n_bootstrap = 100, 
-    weight_scheme = "linear", 
-    precompute_neighbors = TRUE
-  )
-  clq_list[[i]] <- getCLQs(spomic) |> dplyr::mutate(t = time_points_numeric[i], )
-  print(plotSpomic(spomic, point_size = 0.1))
-  print(plotCellProportions(spomic, show_pct = TRUE))
+
+# Process all samples and rewrite to new directory
+main <- function(base_directory, output_directory) {
+  if(!dir.exists(output_directory)) {
+    dir.create(output_directory, recursive = TRUE)
+  }
+  
+  files <-  list.files(base_directory, full.names = TRUE)
+  files_short_name <- list.files(base_directory, full.names = FALSE)
+  pb <- txtProgressBar(min = 1, max = length(files), style = 3)
+  for(i in seq_along(files)) {
+    df <- process_sample(files[i])
+    write.csv(df, 
+              file = file.path(
+                output_directory, 
+                paste0(tools::file_path_sans_ext(files_short_name[i]), ".csv")),
+              row.names = FALSE)
+    setTxtProgressBar(pb, i)
+  }
+  close(pb)
 }
-clqs <- dplyr::bind_rows(clq_list)
-
-# Generate and save plots
-pdf(file = file.path("output", 
-                     "colocalization_vs_time", 
-                     "assembloids_02", 
-                     "colocalization_plots.pdf"), 
-    width = 6.5, 
-    height = 3)
-
-a_b <- unique(clqs$A_B)
-for(i in seq_along(a_b)) {
-  df <- clqs |> filter(A_B == a_b[i])
-  p <- ggplot(df, aes(x = t, y = colocalization_stat, group = 1)) + 
-    geom_point() + 
-    geom_line() + 
-    geom_vline(xintercept =  72, linetype = "dotted", color = "darkgray") + 
-    labs(title = a_b[i], x = "Time (hours)", y = "CLQ") + 
-    theme_pubr() + 
-    theme_publication_text()
-  print(p)
-}
-dev.off()
 
 
+main(base_directory = "data/original_celesta_data/coculture/osimertinib/assembloids_02", 
+     output_directory = "data/coculture/osimertinib/assembloids_02")
 
+main(base_directory = "data/original_celesta_data/coculture/control/assembloids_02", 
+     output_directory = "data/coculture/control/assembloids_02")
 
-
-
-
-
-
-
-
-
-
+# ==============================================================================
